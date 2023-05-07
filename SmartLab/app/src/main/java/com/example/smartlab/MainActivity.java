ae62d2a0ddf871d1360b8dd92e6a1125307f479b
David <davidarrozaqi085@gmail.com>
2023-05-07 23:57:17 +0700

    Finishing Home, Add Edit Profile

--- SmartLab/app/src/main/java/com/example/smartlab/MainActivity.java
+++ SmartLab/app/src/main/java/com/example/smartlab/MainActivity.java
@@ -2,20 +2,23 @@ package com.example.smartlab;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    BottomNavigationView bottomNavigationView;
+public class MainActivity extends AppCompatActivity {
 
     HomeFragment homeFragment = new HomeFragment();
     PeminjamanFragment peminjamanFragment = new PeminjamanFragment();
@@ -25,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         BottomNavigationView bottomNavigationView=(BottomNavigationView)findViewById(R.id.bottom_navigator);
         bottomNavigationView.getMenu().findItem(R.id.home).setChecked(true);
@@ -52,5 +57,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         // code diatas untuk berfungsi memindahkan fragment 1 dan fragment lainya
+
     }
+
+
 }
